ff6beb3a67cb786dd2531b1a69399758983c2791
Renan Tescaro <renantescaro@yahoo.com.br>
2024-02-24 13:14:38 -0300

    feature: create product insert route

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -8,8 +8,24 @@ router.get('/', (req, res) => {
             res.json(result.rows);
         })
         .catch(err => {
-            console.error('Erro ao buscar usuários:', err);
-            res.status(500).json({ error: 'Erro ao buscar usuários' });
+            console.error('Erro ao buscar produtos:', err);
+            res.status(500).json({ error: 'Erro ao buscar produtos' });
+        });
+});
+
+router.post('/', (req, res) => {
+    const { title, brand, description } = req.body;
+
+    const query = 'INSERT INTO products (title, brand, description) VALUES ($1, $2, $3) RETURNING *';
+    const values = [title, brand, description];
+
+    client.query(query, values)
+        .then(result => {
+            res.status(201).json(result.rows[0]);
+        })
+        .catch(err => {
+            console.error('Erro ao inserir produto:', err);
+            res.status(500).json({ error: 'Erro ao inserir produto' });
         });
 });
 
