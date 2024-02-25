d0ae31e810a8e6c68fe0ac98a3301432c2a0bc8f
Renan Tescaro <renantescaro@yahoo.com.br>
2024-02-25 14:46:52 -0300

    feature: link product with category

--- pages/products/edit.js
+++ pages/products/edit.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Link from 'next/link'
+import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../navbar';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://curso-beta-71fca35041cf.herokuapp.com';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://curso-beta-71fca35041cf.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
 
 const EditProductPage = () => {
     const router = useRouter();
@@ -14,6 +15,8 @@ const EditProductPage = () => {
         brand: '',
         description: '',
     });
+    const [categories, setCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
 
     useEffect(() => {
         if (id) {
@@ -23,7 +26,22 @@ const EditProductPage = () => {
                     setProduct(data);
                 })
                 .catch(error => console.error('Erro ao buscar detalhes do produto:', error));
+
+            fetch(`${API_URL}/productsCategories?productId=${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    const selectedIds = data.map(item => item.category_id);
+                    setSelectedCategories(selectedIds);
+                })
+                .catch(error => console.error('Erro ao buscar categorias do produto:', error));
         }
+
+        fetch(`${API_URL}/categories`)
+            .then(response => response.json())
+            .then(data => {
+                setCategories(data);
+            })
+            .catch(error => console.error('Erro ao buscar categorias:', error));
     }, [id]);
 
     const handleChange = (event) => {
@@ -34,23 +52,49 @@ const EditProductPage = () => {
         }));
     };
 
+    const handleCategoryChange = (categoryId) => {
+        // remove categoryId if this in selectedCategories
+        if (selectedCategories.includes(categoryId)) {
+            setSelectedCategories(selectedCategories.filter(id => id !== categoryId));
+        }
+
+        // add categoryId in selectedCategories
+        else {
+            setSelectedCategories([...selectedCategories, categoryId]);
+        }
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch(`${API_URL}/products?id=${id}`, {
+            const productResponse = await fetch(`${API_URL}/products?id=${id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(product),
+                body: JSON.stringify({
+                    ...product,
+                }),
             });
-
-            if (response.ok) {
-                alert('Produto atualizado com sucesso!');
-                router.push(`/products`);
-            } else {
-                alert('Erro ao atualizar o produto. Por favor, tente novamente.');
+            if (!productResponse.ok) {
+                throw new Error('Erro ao atualizar o produto');
             }
+
+            const categoriesResponse = await fetch(`${API_URL}/productsCategories?productId=${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    categories: selectedCategories.filter((item, index) => selectedCategories.indexOf(item) === index),
+                }),
+            });
+            if (!categoriesResponse.ok) {
+                throw new Error('Erro ao atualizar as categorias do produto');
+            }
+
+            alert('Produto atualizado com sucesso!');
+            router.push(`/products`);
         } catch (error) {
             console.error('Erro ao atualizar o produto:', error);
             alert('Erro ao atualizar o produto. Por favor, tente novamente.');
@@ -65,33 +109,51 @@ const EditProductPage = () => {
             <div style={{ padding: "20px" }}>
                 <h1>Editar Produto</h1>
                 <form onSubmit={handleSubmit}>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="title">Título:</label>
-                        <input type="text" id="title" name="title"
-                            className="form-control"
-                            value={product.title} onChange={handleChange} required />
+                    <div className="row">
+                        <div style={{ padding: "20px" }} className="mb-12 col-sm-6">
+                            <div className="row mb-12 col-sm-12">
+                                <label htmlFor="title">Título:</label>
+                                <input type="text" id="title" name="title"
+                                    className="form-control"
+                                    value={product.title} onChange={handleChange} required />
+                            </div>
+                            <div className="row mb-12 col-sm-12">
+                                <label htmlFor="brand">Marca:</label>
+                                <input type="text" id="brand" name="brand"
+                                    className="form-control"
+                                    value={product.brand} onChange={handleChange} required />
+                            </div>
+                            <div className="row mb-12 col-sm-12">
+                                <label htmlFor="description">Descrição:</label>
+                                <textarea id="description" name="description"
+                                    className="form-control"
+                                    value={product.description} onChange={handleChange} required />
+                            </div>
+                            <button type="submit" className='btn btn-primary'>
+                                Atualizar Produto
+                            </button>
+                            <Link className='btn btn-warning' href='/products'>
+                                Cancelar
+                            </Link>
+                        </div>
+                        <div className="mb-12 col-sm-6">
+                            <label>Selecione as categorias:</label>
+                            {categories.map(category => (
+                                <div key={category.id}>
+                                    <input
+                                        type="checkbox"
+                                        id={category.id}
+                                        value={category.id}
+                                        checked={selectedCategories.includes(category.id)}
+                                        onChange={() => handleCategoryChange(category.id)}
+                                    />
+                                    <label htmlFor={category.id}>{category.name}</label>
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="brand">Marca:</label>
-                        <input type="text" id="brand" name="brand"
-                            className="form-control"
-                            value={product.brand} onChange={handleChange} required />
-                    </div>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="description">Descrição:</label>
-                        <textarea id="description" name="description"
-                            className="form-control"
-                            value={product.description} onChange={handleChange} required />
-                    </div>
-                    <button type="submit" className='btn btn-primary'>
-                        Atualizar Produto
-                    </button>
-                    <Link className='btn btn-warning' href='/products'>
-                        Cancelar
-                    </Link>
                 </form>
             </div>
-
         </div>
     );
 };
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,16 +1,29 @@
-import { useState } from 'react';
-import Link from 'next/link'
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from '../navbar';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://curso-beta-71fca35041cf.herokuapp.com';
+// const API_URL = process.env.REACT_APP_API_URL || 'https://curso-beta-71fca35041cf.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
 
 const NewProductForm = () => {
+    const [categories, setCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
     const [formData, setFormData] = useState({
         title: '',
         brand: '',
         description: '',
     });
+
+    useEffect(() => {
+        fetch(`${API_URL}/categories`)
+            .then(response => response.json())
+            .then(data => {
+                setCategories(data);
+            })
+            .catch(error => console.error('Erro ao buscar categorias:', error));
+    }, []);
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -20,28 +33,56 @@ const NewProductForm = () => {
         }));
     };
 
+    const handleCategoryChange = (categoryId) => {
+        if (selectedCategories.includes(categoryId)) {
+            setSelectedCategories(selectedCategories.filter(id => id !== categoryId));
+        } else {
+            setSelectedCategories([...selectedCategories, categoryId]);
+        }
+    };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
-            const response = await fetch(`${API_URL}/products`, {
+            const productResponse = await fetch(`${API_URL}/products`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(formData),
             });
-
-            console.log(response)
-            if (response.ok) {
-                setFormData({
-                    title: '',
-                    brand: '',
-                    description: '',
-                });
-                alert('Produto cadastrado com sucesso!');
-            } else {
-                alert('Erro ao cadastrar o produto. Por favor, tente novamente.');
+            if (!productResponse.ok) {
+                throw new Error('Erro ao cadastrar o produto');
             }
+
+            const productData = await productResponse.json();
+            const productId = productData.id;
+
+            const categoryPromises = selectedCategories.map(categoryId =>
+                fetch(`${API_URL}/productsCategories`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        productId,
+                        categoryId,
+                    }),
+                })
+            );
+            const categoryResponses = await Promise.all(categoryPromises);
+            const categorySuccess = categoryResponses.every(response => response.ok);
+            if (!categorySuccess) {
+                throw new Error('Erro ao associar categorias ao produto');
+            }
+
+            setFormData({
+                title: '',
+                brand: '',
+                description: '',
+            });
+            setSelectedCategories([]);
+            alert('Produto cadastrado com sucesso!');
         } catch (error) {
             console.error('Erro ao cadastrar o produto:', error);
             alert('Erro ao cadastrar o produto. Por favor, tente novamente.');
@@ -53,33 +94,50 @@ const NewProductForm = () => {
             <Navbar />
 
             <form onSubmit={handleSubmit}>
-                <div style={{ padding: "20px" }}>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="title">Titulo:</label>
-                        <input type="text" id="title" name="title"
-                            className="form-control"
-                            value={formData.title} onChange={handleChange} required />
+                <div className="row">
+                    <div style={{ padding: "20px" }} className="mb-12 col-sm-6">
+                        <div className="row mb-12 col-sm-12">
+                            <label htmlFor="title">Titulo:</label>
+                            <input type="text" id="title" name="title"
+                                className="form-control"
+                                value={formData.title} onChange={handleChange} required />
+                        </div>
+                        <div className="row mb-12 col-sm-12">
+                            <label htmlFor="brand">Marca:</label>
+                            <input type="text" id="brand" name="brand"
+                                className="form-control"
+                                value={formData.brand} onChange={handleChange} required />
+                        </div>
+                        <div className="row mb-12 col-sm-12">
+                            <label htmlFor="description">Descrição:</label>
+                            <textarea id="description" name="description"
+                                className="form-control"
+                                value={formData.description} onChange={handleChange} required />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Cadastrar Produto
+                        </button>
+                        <Link className='btn btn-warning' href='/products'>
+                            Cancelar
+                        </Link>
                     </div>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="brand">Marca:</label>
-                        <input type="text" id="brand" name="brand"
-                            className="form-control"
-                            value={formData.brand} onChange={handleChange} required />
+                    <div className="mb-12 col-sm-6">
+                        <label>Selecione as categorias:</label>
+                        {categories.map(category => (
+                            <div key={category.id}>
+                                <input
+                                    type="checkbox"
+                                    id={category.id}
+                                    value={category.id}
+                                    checked={selectedCategories.includes(category.id)}
+                                    onChange={() => handleCategoryChange(category.id)}
+                                />
+                                <label htmlFor={category.id}>{category.name}</label>
+                            </div>
+                        ))}
                     </div>
-                    <div className="row mb-3 col-sm-6">
-                        <label htmlFor="description">Descrição:</label>
-                        <textarea id="description" name="description"
-                            className="form-control"
-                            value={formData.description} onChange={handleChange} required />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Cadastrar Produto
-                    </button>
-                    <Link className='btn btn-warning' href='/products'>
-                        Cancelar
-                    </Link>
                 </div>
-            </form >
+            </form>
         </div>
     );
 };
